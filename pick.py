0bd7fc5b0947d3715c61e123fb46113b1c773a6d
debayant9 <42317228+debayant9@users.noreply.github.com>
2021-06-14 00:50:03 +0530

    Add files via upload

---
+++ pick.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 00:38:14 2021
+
+@author: Debayan
+"""
+
+
+def word_to_chars(word):
+    characters = []
+    for char in word:
+        characters.append(char)
+    return characters 
