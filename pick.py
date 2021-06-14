e92af1561a833abe885b72b11523f63b9c2774e6
debayant9 <42317228+debayant9@users.noreply.github.com>
2021-06-14 22:18:02 +0530

    Update pick.py

--- pick.py
+++ pick.py
@@ -4,10 +4,21 @@ Created on Mon Jun 14 00:38:14 2021
 
 @author: Debayan
 """
+import pickle
 
-
-def word_to_chars(word):
-    characters = []
-    for char in word:
-        characters.append(char)
-    return characters 
+class extract:
+    def word_to_chars(word):
+        characters = []
+        for char in word:
+            characters.append(char)
+        return characters 
+    
+    def tfidf_vectorizer(self):
+        appfile1 = open("vectorizer.pkl","rb")
+        vectorizer = pickle.load(appfile1)
+        return vectorizer
+    
+    def xgboost_model(self):
+        appfile = open("PassCheck_XGBoost.pkl","rb")
+        model = pickle.load(appfile)
+        return model
